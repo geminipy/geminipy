cbf1fcc650150c71eb147e7fb09b787af1d1eff1
Mike M <marzig76@gmail.com>
2016-02-22 21:17:57 -0500

    started working on the trades function

--- geminipy.py
+++ geminipy.py
@@ -23,3 +23,24 @@ class geminipy:
         url = self.base_url + '/v1/book/' + symbol
         response = requests.get(url)
         return response.content
+        
+    def trades(self, symbol, since="", limit_trades=50, include_breaks=False):
+        if symbol == "":
+            return "Error: No symbol specified"
+
+        url = self.base_url + '/v1/trades/' + symbol
+        print url
+        params = {'since':since,'limit_trades':limit_trades,'include_breaks':include_breaks}
+        response = requests.get(url, params)
+        return response.content
+
+testcon = geminipy()
+
+symbolsresponse = testcon.symbols()
+print(symbolsresponse)
+
+#bookresponse = testcon.book('btcusd')
+#print(bookresponse)
+
+#tradesresponse = testcon.trades('btcusd')
+#print(tradesresponse)
