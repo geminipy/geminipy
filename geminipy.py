ff9beb601db30887064d35375cf0269f6c310234
Mike M <marzig76@gmail.com>
2016-02-23 21:54:25 -0500

    started implementing authenticated requests

--- geminipy.py
+++ geminipy.py
@@ -1,14 +1,18 @@
+import json
+import hmac
+import base64
 import requests
+import hashlib
 
 class geminipy:
 
-    live_url = "https://api.gemini.com"
-    sandbox_url = "https://api.sandbox.gemini.com"
+    live_url = 'https://api.gemini.com'
+    sandbox_url = 'https://api.sandbox.gemini.com'
     base_url = sandbox_url
 
     def __init__(self, live = False):
         if live:
-            base_url = self.live_url
+            self.base_url = self.live_url
 
     # public requests
     def symbols(self):
@@ -16,14 +20,37 @@ class geminipy:
         response = requests.get(url)
         return response.content
 
-    def book(self, symbol = "btcusd", limit_bids = 0, limit_asks = 0):
+    def book(self, symbol = 'btcusd', limit_bids = 0, limit_asks = 0):
         url = self.base_url + '/v1/book/' + symbol
         params = {'limit_bids':limit_bids,'limit_asks':limit_asks}
         response = requests.get(url, params)
         return response.content
 
-    def trades(self, symbol = "btcusd", since = 0, limit_trades = 50, include_breaks = 0):
+    def trades(self, symbol = 'btcusd', since = 0, limit_trades = 50, include_breaks = 0):
         url = self.base_url + '/v1/trades/' + symbol
         params = {'since':since,'limit_trades':limit_trades,'include_breaks':include_breaks}
         response = requests.get(url, params)
         return response.content
+
+    # authenticated requests
+    def neworder(self, nonce, amount, price, side, client_order_id = "", symbol = 'btcusd', type = 'exchange limit'):
+        request = '/v1/order/new'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce,
+            "symbol": symbol,
+            "amount": amount,
+            "price": price,
+            "side": side,
+            "type": type
+        }
+
+        if client_order_id != "":
+            params['client_order_id'] = client_order_id
+
+        jsonparams = json.dumps(params)
+        payload = base64.b64encode(jsonparams)
+        signature = hmac.new("secretkey", payload, hashlib.sha384).hexdigest()
+        headers = {"X-GEMINI-APIKEY":"apikey", "X-GEMINI-PAYLOAD":payload, "X-GEMINI-SIGNATURE":signature}
+        return requests.post(url, headers=headers)
