93de7f71f8d9df44e4f7df02afe85d22796ebd83
Mike M <marzig76@gmail.com>
2016-02-24 21:04:26 -0500

    added the rest of the authenticated requests

--- geminipy.py
+++ geminipy.py
@@ -51,8 +51,96 @@ class geminipy:
         if client_order_id is not None:
             params['client_order_id'] = client_order_id
 
+        return requests.post(url, headers=self.prepare(params))
+
+    def cancelorder(self, nonce, order_id):
+        request = '/v1/order/cancel'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce,
+            "order_id": order_id
+        }
+
+        return requests.post(url, headers=self.prepare(params))
+
+    def cancelsession(self, nonce):
+        request = '/v1/order/cancel/session'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce
+        }
+
+        return requests.post(url, headers=self.prepare(params))
+
+    def cancelall(self, nonce):
+        request = '/v1/order/cancel/all'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce
+        }
+
+        return requests.post(url, headers=self.prepare(params))
+
+    def orderstatus(self, nonce, order_id):
+        request = '/v1/order/status'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce,
+            "order_id": order_id
+        }
+
+        return requests.post(url, headers=self.prepare(params))
+
+    def activeorders(self, nonce):
+        request = '/v1/orders'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce
+        }
+
+        return requests.post(url, headers=self.prepare(params))
+
+    def pasttrades(self, nonce, symbol = 'btcusd', limit_trades = 50, timestamp = 0):
+        request = '/v1/mytrades'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce,
+            "symbol": symbol,
+            "limit_trades": limit_trades,
+            "timestamp": timestamp
+        }
+
+        return requests.post(url, headers=self.prepare(params))
+
+    def balances(self, nonce):
+        request = '/v1/balances'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce
+        }
+
+        return requests.post(url, headers=self.prepare(params))
+
+    def heartbeat(self, nonce):
+        request = '/v1/heartbeat'
+        url = self.base_url + request
+        params = {
+            "request": request,
+            "nonce": nonce
+        }
+
+        return requests.post(url, headers=self.prepare(params))
+
+    def prepare(self, params):
         jsonparams = json.dumps(params)
         payload = base64.b64encode(jsonparams)
         signature = hmac.new(self.secretkey, payload, hashlib.sha384).hexdigest()
-        headers = {"X-GEMINI-APIKEY":self.apikey, "X-GEMINI-PAYLOAD":payload, "X-GEMINI-SIGNATURE":signature}
-        return requests.post(url, headers=headers)
+
+        return {"X-GEMINI-APIKEY":self.apikey, "X-GEMINI-PAYLOAD":payload, "X-GEMINI-SIGNATURE":signature}
