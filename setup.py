f2f225d37ab7b1c5b8d8b97c889d3097d8b1d619
godmin <umpierrez@pm.me>
2021-11-01 03:48:22 +0000

    Code organization by creating "utils.py" and "core.py" files. Some errors fixed on README examples. Using find_packages in "setup.py" file.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup
 from pathlib import Path
 
 readme = Path('README.md')
@@ -10,8 +10,8 @@ else:
 
 setup(
     name='geminipy',
-    version='0.0.6',
-    packages=find_packages('geminipy'),
+    version='0.0.5',
+    packages=['geminipy'],
     url='https://github.com/pl0mo/geminipy',
     license='GNU GPL',
     author='Mike Marzigliano',
