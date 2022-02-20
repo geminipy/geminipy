a18faae9252edb712ca8689dedfd455e6bf56c04
SalihSoleh <salihsoleh@zohomail.com>
2022-02-20 10:45:52 +0000

    Added Any from typing module.

--- geminipy/utils.py
+++ geminipy/utils.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from typing import Dict
+from typing import Any, Dict
 
 
-def _get_params(params: Dict, *exclude) -> Dict:
+def _get_params(params: Dict, *exclude) -> Dict[str, Any]:
     check = lambda k, v: v is not None and k[0].isalpha() and k not in exclude
     return {k: v for k, v in params.items() if check(k, v) and k != 'self'}
 
